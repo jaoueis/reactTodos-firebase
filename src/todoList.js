80a42128376e0d61a95026010d69b26954ef8c13
Shan Jiang <jiang_shan@live.com>
2018-09-21 16:56:52 -0400

    I did a mess

--- src/todoList.js
+++ src/todoList.js
@@ -1,37 +1,58 @@
-import React, { Component } from "react";
+import React from "react";
+import ReactDOM from "react-dom";
 
-class TodoList extends Component {
-constructor(props) {
-    super(props);
+class TodoList extends React.Component {
+  constructor (props) {
+    super(props)
 
     this.state = {
-        items:[]
-    };
+      text: '',  
+      items: []
+    }
+  }
 
-    this.addTask = this.addItem.bind(this);
+  handleChange (e) {
+    this.setState({text: e.target.value})
+  }
+
+  addTask (e){
+      this.setState({
+          text:'',
+          items:[this.state.items]
+      });
+  }
+
+  render () {
+    return (
+      <div className='todoListMain'>
+        <div className='header'>
+          <form onSubmit={this.addTask}>
+            <input
+              id='new-todo'
+              onChange={this.handleChange}
+              value={this.state.text}
+              placeholder='Enter task...'>
+            </input>
+            <button type='submit'>
+              Add
+            </button>
+          </form>
+        </div>
+      </div>
+    )
+  }
 }
 
-addTask(e){
-    
-}
-
-    render() {
-        return (
-            <div className="todoListMain">
-                <div className="header">
-                    <form onSubmit={this.addTask}>
-                        <input id="new-todo" onChange={this.handleChange} value={this.state.text} placeholder="Enter task...">
-                        </input>
-                        <button type="submit">Add</button>
-                    </form>
-                </div>
-            </div>
+class todoLists extends React.Component  {
+    render (){
+        return(
+            <ul>
+                {this.props.items.map(item => (
+                    <li>{item.text}</li>
+                ))}
+            </ul>
         );
     }
-
-    handleChange(e){
-        this.setState({text: e.target.value});
-    }
 }
 
-export default TodoList;
+export default TodoList
