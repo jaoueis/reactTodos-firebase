f68c64d92c683961e2dcd546a8713f1d2f96f3a0
Shan Jiang <jiang_shan@live.com>
2018-09-24 11:50:09 -0400

    finished app learning

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 ##### Run
 `npm run start` to preview project
 
+##### Read the following before learning this project
+- `https://reactjs.org/docs/rendering-elements.html`
+- `https://reactjs.org/docs/components-and-props.html`
+- `https://reactjs.org/docs/state-and-lifecycle.html`
+- `https://reactjs.org/docs/handling-events.html`
+
 ##### Resources
 - https://reactjs.org/
 - https://www.kirupa.com/react/simple_todo_app_react.htm 
--- src/todoList.js
+++ src/todoList.js
@@ -1,58 +1,58 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
-class TodoList extends React.Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      text: '',  
-      items: []
-    }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [], text: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange (e) {
-    this.setState({text: e.target.value})
-  }
-
-  addTask (e){
-      this.setState({
-          text:'',
-          items:[this.state.items]
-      });
-  }
-
-  render () {
+  render() {
     return (
       <div className='todoListMain'>
         <div className='header'>
-          <form onSubmit={this.addTask}>
-            <input
-              id='new-todo'
-              onChange={this.handleChange}
-              value={this.state.text}
-              placeholder='Enter task...'>
-            </input>
-            <button type='submit'>
-              Add
-            </button>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" placeholder="Enter new task here..." id="new-todo" value={this.state.text} onChange={this.handleChange} />
+            <button type="submit">Add Task #{this.state.items.length + 1}</button>
           </form>
         </div>
+        <TodoList items={this.state.items} />
       </div>
-    )
+    );
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.text.length) {
+      return;
+    } else {
+      const newItem = {
+        text: this.state.text,
+        id: Date.now()
+      };
+      this.setState(state => ({
+        items: state.items.concat(newItem),
+        text: ''
+      }));
+    }
   }
 }
 
-class todoLists extends React.Component  {
-    render (){
-        return(
-            <ul>
-                {this.props.items.map(item => (
-                    <li>{item.text}</li>
-                ))}
-            </ul>
-        );
-    }
+class TodoList extends React.Component {
+  render() {
+    return (
+      <ol>
+        {this.props.items.map(item => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ol>
+    );
+  }
 }
 
-export default TodoList
+export default App
