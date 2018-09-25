5a48d437fa3aefdad908d74672328832cb29a3da
Shan Jiang <jiang_shan@live.com>
2018-09-25 15:13:23 -0400

    delete firebase api for security reason

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,6 @@
 import firebase from "firebase";
 
 // Initialize Firebase
-var config = {
-  apiKey: "AIzaSyDTRIIsaUU9nqCdXXqnLgdr2wb38KQ5VL4",
-  authDomain: "react-todo-list-38fd1.firebaseapp.com",
-  databaseURL: "https://react-todo-list-38fd1.firebaseio.com",
-  projectId: "react-todo-list-38fd1",
-  storageBucket: "react-todo-list-38fd1.appspot.com",
-  messagingSenderId: "667715307593"
-};
-firebase.initializeApp(config);
+// Your Firebase config code snippet does here
 
 export default firebase;
